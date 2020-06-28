69cb3755360d7cf7d9f49a978a6011bccbd29114
michedomingo <michedomingo@gmail.com>
2020-06-28 21:25:24 +0000

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,11 @@ This module define class BaseModel
 import json
 import uuid
 import os
+<<<<<<< HEAD
 from datetime import datetime, date, time
+=======
+from datetime import datetime
+>>>>>>> 19afae46fb63fe70b160282ddad688b415715ec4
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
@@ -14,5 +18,15 @@ class BaseModel:
     def __init__(self):
         """Initialize a BaseModel class"""
         self.id = str(uuid.uuid4())
+<<<<<<< HEAD
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+=======
+        #assign created_at
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        #return the string version of BaseModel
+        return ("[{}] ({}) {}".
+                format(self.__class__.__name__,self.id, self.__dict__))
+>>>>>>> 19afae46fb63fe70b160282ddad688b415715ec4
