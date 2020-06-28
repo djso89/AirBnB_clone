81580c8aed3cbecf1af0ccd8f53f6da4e6066e67
michedomingo <michedomingo@gmail.com>
2020-06-28 21:05:59 +0000

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-Base model module
+This module define class BaseModel
 """
 
 import json
 import uuid
 import os
-
+from datetime import datetime, date, time
 
 class BaseModel:
-    """Base class """
+    """Defines all common attributes/methods for other classes"""
 
     def __init__(self):
-        # generate unique id and convert to string
-        #convert uid to string
+        """Initialize a BaseModel class"""
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
