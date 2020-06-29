0e9460052830155e406b896f04f1ec3a97209f5f
michedomingo <michedomingo@gmail.com>
2020-06-29 11:27:35 -0700

    Task 8: Write a class User that inherits from BaseModel

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/use/bin/python3
+"""
+This module define class User
+"""
+from models.base_model import BaseModel
+
+
+class User:
+    """class User that inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
