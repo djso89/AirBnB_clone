ff1598767c92899d20b4596daf66c9037042d86b
michedomingo <michedomingo@gmail.com>
2020-06-30 01:16:50 +0000

    WIP do_all

--- console.py
+++ console.py
@@ -2,21 +2,41 @@
 """
 Contains the entry point of the command interpreter
 """
-
 import cmd
+from models.base_model import BaseModel
+import models
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter"""
     prompt = '(hbnb) '
 
+
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances
+        based or not on the class name
+        """
+        if not line or line == 'BaseModel':
+            print(str(value) for key, value in storage.all().items()])
+        else:
+            print("** class doesn't exist **")
+
     def do_EOF(self, line):
         """EOF command to exit the program"""
+        print()
         return True
 
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
+
+    def emptyline(self):
+        """
+        Do nothing
+        """
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
