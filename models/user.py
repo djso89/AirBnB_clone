4c13d79e872cf0ba4a42e1140fd7ba57994dea3d
michedomingo <michedomingo@gmail.com>
2020-07-01 11:08:33 -0700

    Update doctring

--- models/user.py
+++ models/user.py
@@ -6,7 +6,13 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class defines User information"""
+    """
+    Initialize class User with user information
+        email: (str) user's email
+        password: (str) user's password
+        first_name: (str) user's first name
+        last_name: (str) user's last name
+    """
     email = ""
     password = ""
     first_name = ""
